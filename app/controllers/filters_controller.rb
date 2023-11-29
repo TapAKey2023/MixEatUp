e5e5b5db80e1f2d14e6a542e9c69966333deaaaa
Visar Sabani <vissarshabani@gmail.com>
2023-11-29 18:03:39 +0000

    Cookies

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,20 +2,82 @@ class FiltersController < ApplicationController
   def index
   end
 
+  def occasion
+  end
+
   def meal
+    cookies[:occasion] = params[:occasion]
   end
 
   def evenet
   end
 
   def clarifications
+    cookies[:meal] = params[:meal]
   end
 
-  def location
+  def user_clarification_choice
+    choices = params[:user_clarifications]
+    if choices[:budget] == "1"
+      cookies[:budget] = "positive"
+    else
+      cookies[:budget] = "negative"
+    end
+
+    if choices[:preferences] == "1"
+      cookies[:preferences] = "positive"
+    else
+      cookies[:preferences] = "negative"
+    end
+
+    if choices[:location] == "1"
+      cookies[:location] = "positive"
+    else
+      cookies[:location] = "negative"
+
+    end
+
+    redirect_to filters_budget_path
   end
 
   def budget
+    # if cookies[:budget] == "negative"
+    #   redirect_to filters_preferences_path
+    #   return
+    # end
+    redirect_to filters_preferences_path if cookies[:budget] == "negative"
+
+    # if params[:budget] == "positive"
+    #   cookies[:budget] = params[:budget]
+    # end
+    # if params[:preferences] == "positive"
+    #   cookies[:preferences] = params[:preferences]
+    # end
+    # if params[:location] == "positive"
+    #   cookies[:location] = params[:location]
+    # end
+    # raise
   end
 
+  def preferences
+    if cookies[:budget] == "positive"
+      cookies[:total_price] = params[:my_method][:total_price]
+      cookies[:number_of_people] = params[:my_method][:number_of_people]
+    end
 
+    redirect_to filters_location_path if cookies[:preferences] == "negative"
+  end
+
+  def location
+    if cookies[:preferences] == "positive"
+      cookies[:whear] = params[:wheat]
+      cookies[:lactose] = params[:lactose]
+      cookies[:nuts] = params[:nuts]
+      cookies[:vegetarian] = params[:vegetarian]
+    end
+
+    if cookies[:location] == "negative"
+      redirect_to restaurants_loading_path
+    end
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,22 @@ class RestaurantsController < ApplicationController
   end
 
   def loading
+    if cookies[:location] == "positive"
+      cookies[:city] = params[:my_method][:City]
+      cookies[:radius] = params[:my_method][:Radius]
+      end
+    # if params[:my_method][:wheat] == "positive"
+    #   cookies[:wheat] = params[:my_method][:wheat]
+    # end
+    # if params[:my_method][:lactose] == "positive"
+    #   cookies[:lactose] = params[:my_method][:lactose]
+    # end
+    # if params[:my_method][:nuts] == "positive"
+    #   cookies[:nuts] = params[:my_method][:nuts]
+    # end
+    # if params[:my_method][:vegetarian] == "positive"
+    #   cookies[:vegetarian] = params[:my_method][:vegetarian]
+    # end
   end
 
   def saved
