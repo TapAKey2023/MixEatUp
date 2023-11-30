7626b210654201a93f55962d4fe13b703e44f7f0
Ecem Kocaslan <ecemkocaslan06@hotmail.com>
2023-11-30 18:02:30 +0000

    new-routes

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -16,35 +16,91 @@ class FiltersController < ApplicationController
     cookies[:meal] = params[:meal]
   end
 
-  def user_clarification_choice
-    choices = params[:user_clarifications]
-    if choices[:budget] == "1"
-      cookies[:budget] = "positive"
-    else
-      cookies[:budget] = "negative"
-    end
-
-    if choices[:preferences] == "1"
-      cookies[:preferences] = "positive"
-    else
-      cookies[:preferences] = "negative"
-    end
-
-    if choices[:location] == "1"
-      cookies[:location] = "positive"
-    else
-      cookies[:location] = "negative"
-
-    end
-
-    redirect_to filters_budget_path
+  def budget_og
+    cookies[:index] = params[:index]
   end
+
+  def meal_og
+    cookies[:total_price_og] = params[:my_method][:total_price_og]
+    cookies[:number_of_people_og] = params[:my_method][:number_of_people_og]
+  end
+
+  def clarifications_og
+    cookies[:meal_og] = params[:meal_og]
+  end
+
+
+  def user_clarification_choice
+    # if cookies[:index] == "filter-occasion"
+
+    choices = params[:user_clarification_choice]
+      if choices[:budget] == "1"
+        cookies[:budget] = "positive"
+      else
+        cookies[:budget] = "negative"
+      end
+
+      if choices[:preferences] == "1"
+        cookies[:preferences] = "positive"
+      else
+        cookies[:preferences] = "negative"
+      end
+
+      if choices[:location] == "1"
+        cookies[:location] = "positive"
+      else
+        cookies[:location] = "negative"
+      end
+
+      redirect_to filters_budget_path
+    # end
+  end
+
+
+  def user_clarification_choice_og
+    cookies[:preferences_og] = "negative"
+    cookies[:location_og] = "negative"
+
+    if cookies[:index] == "filter-budget"
+    choices = params[:user_clarifications_og]
+    if choices[:preferences_og] == "1"
+      cookies[:preferences_og] = "positive"
+    else
+      cookies[:preferences_og] = "negative"
+    end
+
+    if choices[:location_og] == "1"
+      cookies[:location_og] = "positive"
+    else
+      cookies[:location_og] = "negative"
+    end
+
+    if cookies[:preferences_og] == "positive"
+      redirect_to filters_preferences_path
+      return
+    end
+
+    if cookies[:location_og] == "negative"
+     redirect_to restaurants_loading_path
+     return
+    else
+      redirect_to filters_location_path
+      return
+    end
+  end
+  end
+
+
+
+
+
 
   def budget
     # if cookies[:budget] == "negative"
     #   redirect_to filters_preferences_path
     #   return
     # end
+    cookies[:index] = params[:index]
     redirect_to filters_preferences_path if cookies[:budget] == "negative"
 
     # if params[:budget] == "positive"
@@ -60,16 +116,27 @@ class FiltersController < ApplicationController
   end
 
   def preferences
-    if cookies[:budget] == "positive"
-      cookies[:total_price] = params[:my_method][:total_price]
-      cookies[:number_of_people] = params[:my_method][:number_of_people]
-    else
-      cookies[:total_price] = "negative"
-      cookies[:number_of_people] = "negative"
-    end
+    # if cookies[:index] == "filter-occasion"
+      if cookies[:budget_og] == "positive"
+        cookies[:total_price_og] = params[:my_method][:total_price_og]
+        cookies[:number_of_people_og] = params[:my_method][:number_of_people]
+      else
+        cookies[:total_price_og] = "negative"
+        cookies[:number_of_people_og] = "negative"
+      end
 
-    redirect_to filters_location_path if cookies[:preferences] == "negative"
+      if cookies[:budget] == "positive"
+        cookies[:total_price] = params[:my_method][:total_price]
+        cookies[:number_of_people] = params[:my_method][:number_of_people]
+      else
+        cookies[:total_price] = "negative"
+        cookies[:number_of_people] = "negative"
+      end
+      redirect_to filters_location_path if cookies[:preferences] == "negative"
+      return
+    # end
   end
+
 
   def location
     if cookies[:preferences] == "positive"
@@ -84,8 +151,14 @@ class FiltersController < ApplicationController
       cookies[:vegetarian] = "negative"
     end
 
+    if cookies[:location_og] == "negative"
+      redirect_to restaurants_loading_path
+      return
+    end
+
     if cookies[:location] == "negative"
       redirect_to restaurants_loading_path
+      return
     end
   end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,23 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
-    if cookies[:location]
       # TODO: filter by location
+      # total price
+      final_price = cookies[:total_price] || cookies[:total_price_og]
+      # number of people
+      final_number_of_people = cookies[:number_of_people] || cookies[:number_of_people_og]
+      # city
+      final_city = cookies[:city]
+      # allergies
+      final_allergies_wheat = cookies[:wheat]
+      final_allergies_lactose = cookies[:lactose]
+      final_allergies_nuts = cookies[:nuts]
+      final_allergies_vegetarian = cookies[:vegetarian]
+
+      if cookies[:location] == "positive" || cookies[:location_og] == "positive"
       @restaurants = @restaurants.where(address: cookies[:city])
+
+      #please add the same block of code for other cookies
     end
     # if cookies[:event]
     #   @restaurants = @restaurants.where(event: cookies[:event])
@@ -17,10 +31,15 @@ class RestaurantsController < ApplicationController
   end
 
   def loading
+
     @restaurants = Restaurant.all
-    if cookies[:location] == "positive"
+    if cookies[:location] == "positive" || cookies[:location_og] == "positive"
+      raise
       cookies[:city] = params[:my_method][:City]
       cookies[:radius] = params[:my_method][:Radius]
+    else
+      cookies[:city] = "negative"
+      cookies[:radius] = "negative"
     end
     # if params[:my_method][:wheat] == "positive"
     #   cookies[:wheat] = params[:my_method][:wheat]
