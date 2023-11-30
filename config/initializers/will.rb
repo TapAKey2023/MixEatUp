cbfe52cb9902a866460cbaa565905ef5f1daa2bf
Claire Ross <cross2021@gmail.com>
2023-11-30 17:36:29 +0000

    f

--- config/initializers/will.rb
+++ config/initializers/will.rb
@@ -1,3 +1,4 @@
-def will_image_tag(key)
-  return "<img src='http://res.cloudinary.com/dgosrthsf/image/upload/#{key}' alt='wills special image'>".html_safe
+def will_image_tag(key, options = {})
+  classNames = options[:class] ? options[:class] : ""
+  return "<img class='#{classNames}' src='http://res.cloudinary.com/dgosrthsf/image/upload/#{key}' alt='wills special image'>".html_safe
 end
