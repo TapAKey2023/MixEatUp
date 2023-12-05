73fcd9de41f6ef1b2ac87ccf541a1c9cb873fad5
Claire Ross <cross2021@gmail.com>
2023-12-05 00:38:38 +0000

    updated changes to budget

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+   cookies.delete :preferences
+   cookies.delete :occasion
+   cookies.delete :meal
+   cookies.delete :location
+   cookies.delete :index
+   cookies.delete :clarifications
+   cookies.delete :budget
+   cookies.delete :budget_clarifications
   end
 
   def test_page
